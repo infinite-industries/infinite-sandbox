3e9543af73db61bf7d4edb16de2c016210040931
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2019-04-14 15:17:48 -0400

    Add CORS support to API server

--- api-server/server.js
+++ api-server/server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,HEAD,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Allow-Credentials', 'true')
+    next()
+})
+
 app.use(express.static('public'))
 
 app.listen(port, () => console.log(`server ready: ${port}`))
